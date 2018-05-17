4a4c6409cd22bc47c14c52dac4958738e2ae18af
Beatrice Mendoza <mendoza@nyu.edu>
2018-05-17 19:58:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,5 +5,10 @@ const playlist = {
 };
 
 function updatePlaylist(playlist, artist, song) {
-  
+  playlist[artist] = song;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artist) {
+  return 
 }
